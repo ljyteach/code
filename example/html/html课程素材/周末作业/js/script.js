32ece9efa78ddc021ab0f601d989d4392567d94c
lee <jiyangli@163.com>
2016-05-05 09:47:30 +0800

    修改

--- example/html/html课程素材/周末作业/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function() { 
-	$('.list-services a')
-	.hover(function(){
-		$(this).stop().animate({opacity:.5}, {duration:300})
-	}, function(){
-		$(this).stop().animate({opacity:1}, {duration:300})
-	});
-}); 
