182121f3e5d8c592823ba681b4caf4015c5416a1
lee <jiyangli@163.com>
2016-05-16 23:38:49 +0800

    不知道修改哪了

---
+++ test/webApp/script/fontSizeSet.js
@@ -0,0 +1,13 @@
+  (function (doc, win) {
+  var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';  //640位设计稿的宽度，64是html font-size属性的值
+    };
+
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
